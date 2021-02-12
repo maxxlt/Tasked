51dfd527d2f17a77d66c106e5235f96e37c798f5
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2021-02-11 18:48:28 -0600

    amplify cli added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import {withAuthenticator} from 'aws-amplify-react-native';
+
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
-import {withAuthenticator} from 'aws-amplify-react-native';
 
 const App = () => {
   return (
@@ -24,4 +26,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
