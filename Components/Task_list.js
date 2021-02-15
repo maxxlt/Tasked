f0a82f76f14f36e4b8815c8d40f90cdf8a5ce495
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2021-02-15 16:06:00 -0600

    Task_List component

---
+++ Components/Task_list.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+
+const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+
+  
+const Task_list = (props) => {
+    const renderItem = ({ item }) => (
+        <Item title={item.title} />
+      );
+  return (
+        <FlatList
+        data={props.Data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        />
+ 
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+      },
+      item: {
+        backgroundColor: '#FCCF3E',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius:20
+      },
+      title: {
+        fontSize: 32,
+      },
+    });
+
+export default Task_list;
