85d62e28f616f95c136eea160b72a135c0df3e8d
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2021-02-15 17:08:37 -0600

    profile img

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 
 const  Profile = (props) => {
   return (
-    <View style={styles.container}><Text>Profile Page</Text></View>
+    <View style={styles.container}>
+       <Image style ={styles.profileimg} source={require('../assets/profile_ig.jpg')}></Image>
+        <Text>Krishna Khadka</Text>
+      <Image style={styles.img} source={require('../assets/orange-wave.png')}>
+       
+      </Image>
+     
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex:1,
+    alignItems:"center",
+    alignContent:"center"
+    
+  },
+  img:{
+    width:400,
+    height:210,  
+    
+  },
+  profileimg:{
+    width:100,
+    height:100,
+    borderRadius:50,
+    marginTop:10
+  
+    
+  }
 });
 
 export default Profile;
