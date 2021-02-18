1149be963d9de36757e5e1e9254cb41942a21b8d
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2021-02-18 12:00:41 -0600

    bottom tab push

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {withAuthenticator} from 'aws-amplify-react-native';
-import Amplify from 'aws-amplify';
+import Amplify from '@aws-amplify/core';
 import config from './aws-exports';
+import {
+   
+   API,
+   graphqlOperation,
+ } from 'aws-amplify';
 Amplify.configure(config);
 import NavContainer from './Screens/Navigation/NavContainer';
-
+import {getUser} from './src/graphql/queries';
+import {createUser} from './src/graphql/mutations';
 
 const App = () => {
+   
   return (
       <NavContainer/>
    );
