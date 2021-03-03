13612504b743fdf3089d5c7a8dcf0239bc949a26
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2021-03-03 02:48:21 -0600

    group background color

--- components/Group.js
+++ components/Group.js
@@ -60,7 +60,6 @@ const Group = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
