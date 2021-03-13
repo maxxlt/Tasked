42fe37c2770afb61a70242513b4aaf191818b3c2
maxxlt <din.chan@mavs.uta.edu>
2021-03-12 19:45:32 -0600

    changed bottom nav color

--- components/Appbar.js
+++ components/Appbar.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View,Image } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 const Appbar = (props) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>{props.title}</Text>
-            <Image style ={styles.image} source ={require('../assets/img/android/logo-black-text.png')}></Image>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{props.title}</Text>
+    </View>
+  );
+};
 
-export default Appbar
+export default Appbar;
 
 const styles = StyleSheet.create({
-    container:{
-        //flex:1,
-        alignContent:'center',
-        alignItems:'center',
-        backgroundColor:'#FCCF3E',
-        height:90,
-        marginTop:40,
-        borderBottomLeftRadius:15,
-        borderBottomRightRadius:15,
-        marginBottom:15
-    },
-    image:
-    {height:40, width:40, borderRadius:5},
-    text:{
-        padding:5
-    }
-})
+  container: {
+    //flex:1,
+    alignContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FCCF3E",
+    height: 64,
+    marginTop: 40,
+    marginBottom: 15,
+  },
+  image: { height: 40, width: 40, borderRadius: 5 },
+  text: {
+    marginTop: 25,
+    color: "#FFFFFF",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
--- navigation/Group_Task_Navigator.js
+++ navigation/Group_Task_Navigator.js
@@ -2,42 +2,53 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Group from "../components/Group";
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import MyTask from '../components/MyTask';
+import AntDesign from "react-native-vector-icons/AntDesign";
+import MyTask from "../components/MyTask";
 import Profile from "../components/Profile";
 
 const Tab = createBottomTabNavigator();
 
 const Group_Task_Navigator = (props) => {
   return (
-    
-      <Tab.Navigator >
+    <Tab.Navigator
+      tabBarOptions={{
+        inactiveBackgroundColor: "#646669",
+        activeBackgroundColor: "#646669",
+        activeTintColor: "#FFFFFF",
+        inactiveTintColor: "#8B8B8B",
+      }}
+    >
       <Tab.Screen
-          name={"HomeScreen"}
-          component ={MyTask}
-          options={{
-          tabBarLabel: 'MyTask',
+        name={"HomeScreen"}
+        component={MyTask}
+        options={{
+          tabBarLabel: "MyTask",
           tabBarIcon: ({ color }) => (
-         <AntDesign name="checkcircleo" color={'#FCCF3E'} size={23} />
-            ),
-          }}/>
-        <Tab.Screen name="Group" component={Group} options={{
-          tabBarLabel: 'Groups',
+            <AntDesign name="checkcircle" color={color} size={23} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Group"
+        component={Group}
+        options={{
+          tabBarLabel: "Groups",
           tabBarIcon: ({ color }) => (
-          <AntDesign name="appstore-o" color={'#FCCF3E'} size={23} />
-            ),
-          }} />
-         <Tab.Screen
-          name={"Profile"}
-          component ={Profile}
-          options={{
-            tabBarLabel: 'Profile',
-            tabBarIcon: ({ color }) => (
-            <AntDesign name="user" color={'#FCCF3E'} size={23} />
-            ),
-          }}/> 
-      </Tab.Navigator>
- 
+            <AntDesign name="appstore-o" color={color} size={23} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={"Profile"}
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="user" color={color} size={23} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 export default Group_Task_Navigator;
