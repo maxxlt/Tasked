70a178001c9a31e884bd82963bf0299abe4a0996
devasriwarrier <43079678+devasriwarrier@users.noreply.github.com>
2021-03-22 21:06:09 -0500

    comments to backend

--- backend/FirestoreUpdateGroup.js
+++ backend/FirestoreUpdateGroup.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { db } from "../config/firebase";
 //update group
-const FirestoreUpdateGroup = (selectedGroupId, groupName) => {
+const FirestoreUpdateGroup = (selectedGroupId, groupName) => { //function to update groupnames
   db.collection("groups").doc(selectedGroupId).update({
     group_name: groupName,
   });
