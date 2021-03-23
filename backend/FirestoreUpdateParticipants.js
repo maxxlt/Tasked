1e3bacdd2f2116acb8da97054c270ffde1f5f009
devasriwarrier <43079678+devasriwarrier@users.noreply.github.com>
2021-03-22 21:06:50 -0500

    small comment to update participants

--- backend/FirestoreUpdateParticipants.js
+++ backend/FirestoreUpdateParticipants.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { arrayToUpdate, db } from "../config/firebase";
 //update participants
-const FirestoreUpdateParticipants = (newParticipantId, groupId) => {
+const FirestoreUpdateParticipants = (newParticipantId, groupId) => { //function passing in participant ID and group ID to update newParticipantID array
   db.collection("groups")
     .doc(groupId)
     .update({
