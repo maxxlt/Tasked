9b22b5fdf73bac54b9a4a6e5e1a78a50b91acabf
sam3le <samuel.le@mavs.uta.edu>
2021-04-21 16:22:15 -0500

    copied color changes from master

--- assets/color.js
+++ assets/color.js
@@ -3,11 +3,7 @@ const colors = {
   white: "#FFFFFF",
   orange: "#FCCF3E",
   logoorange: "#FFC038",
-<<<<<<< HEAD
-  gray: "#DDDDDD",
-=======
   grey: "#bdbdbd",
   success: "#76ff03",
->>>>>>> 4ab02140e4bfe54a66c7b145876ce5a572dbb26a
 };
 export default colors;
