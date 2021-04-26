359b53d8975f26f27d388ac48151c48f1a0b9c25
maxxlt <din.chan@mavs.uta.edu>
2021-04-26 02:37:16 -0500

    pull down to update implemented

--- components/MyTask.js
+++ components/MyTask.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, ScrollView, ActivityIndicator, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  ActivityIndicator,
+  Text,
+  RefreshControl,
+} from "react-native";
 import Task_list from "./Task_list.js";
 import Appbar from "./Appbar";
 import { auth } from "../config/firebase";
@@ -8,15 +15,27 @@ import FirestoreQueryMyTasks from "../backend/FirestoreQuerryMyTasks";
 import registerForPushNotificationsAsync from "../backend/Notification";
 import Colors from "../assets/color";
 
+const wait = (timeout) => {
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+};
+
 const MyTask = () => {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = React.useState(false);
+
   //Rerenders the component every single time when database for groups is changed
   if (auth.currentUser) {
     registerForPushNotificationsAsync().then((token) =>
       FirestoreUpdatePushToken(token)
     );
   }
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
   useEffect(() => {
     setLoading(true);
     let subscriber = () => {
@@ -32,26 +51,35 @@ const MyTask = () => {
     // Unsubscribe from events when no longer in use
 
     return () => subscriber();
-  }, []);
+  }, [refreshing]);
   if (loading) {
     return <ActivityIndicator />;
   }
-  if(tasks.length == 0)
-  {
-    return(
+  if (tasks.length == 0) {
+    return (
       <View style={styles.container}>
-      <Appbar title="Tasks" />
-      <View style={{alignItems:"center", justifyContent:"center", alignContent:"center",flex:1}}>
-
-        <Text style = {{color:"grey", size:20}}>No Tasks Assigned</Text>
+        <Appbar title="Tasks" />
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            alignContent: "center",
+            flex: 1,
+          }}
+        >
+          <Text style={{ color: "grey", size: 20 }}>No Tasks Assigned</Text>
+        </View>
       </View>
-    </View>
-    )
+    );
   }
   return (
     <View style={styles.container}>
       <Appbar title="Tasks" />
-      <ScrollView>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
         <Task_list Data={tasks} />
       </ScrollView>
     </View>
